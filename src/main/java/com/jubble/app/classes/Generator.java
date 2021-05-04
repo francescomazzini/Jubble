3196156d23bb5b1dbb7a9d04f50085d83f2dfcbf
Francesco <francesco.mazzini432@gmail.com>
2021-05-04 17:11:04 +0200

    Update Generator Class

--- src/main/java/com/jubble/app/classes/Generator.java
+++ src/main/java/com/jubble/app/classes/Generator.java
@@ -3,8 +3,27 @@ package com.jubble.app.classes;
 public class Generator {
 
     private String name;
-    private int nunmberOwned;
-    private double balanceGenerator;
-    private double priceGenerator;
+    private int numberOwned;
+    private int multiplier;
+    private final double PRODUCTIONBASE;
+    private final double COSTBASE;
+
+    private static final double RATEGROWTH = 1.07;
+
+    public Generator(String name, double COSTBASE, double PRODUCTIONBASE) {
+        numberOwned = 0;
+        multiplier = 1;
+        this.name = name;
+        this.COSTBASE = COSTBASE;
+        this.PRODUCTIONBASE = PRODUCTIONBASE;
+    }
+
+    public double getNextCost() {
+        return COSTBASE * Math.pow(RATEGROWTH, numberOwned);
+    }
+
+    public double getProduction() {
+        return (PRODUCTIONBASE * numberOwned) * multiplier;
+    }
 
 }
