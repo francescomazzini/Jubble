ac89f841a72b9ddb9fd37f2b3664bf9d542159a8
Alberto Defendi <1369-ahl-berto@users.noreply.gitlab.inf.unibz.it>
2021-05-07 09:37:28 +0200

    Move game loop to separate file.

--- src/main/java/com/jubble/app/App.java
+++ src/main/java/com/jubble/app/App.java
@@ -11,14 +11,7 @@ public class App {
    * Start game main loop
    * */
   public static void main(String[] args) {
-    Timer timer = new Timer();
-    IncrementValues game = new IncrementValues();
-
-    Observable observe = new Observable();
-    GameAction action = new Observer();
-    observe.attach(action);
-    observe.setAction("State1");
-    observe.setAction("State2");
-    timer.schedule(game, 0, 1000);
+    GameRunner runnable = new GameRunner();
+    runnable.run();
   }
 }
