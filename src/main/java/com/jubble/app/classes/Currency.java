d4f9085a0b9c4f36f5f0b219beb22c6d5e7985ea
Alberto Defendi <1369-ahl-berto@users.noreply.gitlab.inf.unibz.it>
2021-05-11 08:59:33 +0200

    Document.

--- src/main/java/com/jubble/app/classes/Currency.java
+++ src/main/java/com/jubble/app/classes/Currency.java
@@ -4,15 +4,26 @@ public class Currency {
     private String name;
     private double owned;
 
+    /**
+     * This represent the currency of the game.
+     * @param name of the currency.
+     * */
     public Currency(String name) {
         this.name = name;
         owned = 0;
     }
 
+    /**
+     * Set amount of owned currency.
+     * @param owned number of currency to set.
+     * */
     public void setOwned(double owned) {
         this.owned = owned;
     }
 
+    /**
+     * Get amount of owned currency.
+     * */
     public double getOwned() {
         return owned;
     }
