e1a8178fd5659e7156038ab98bf4e8456ca09421
Alberto Defendi <1369-ahl-berto@users.noreply.gitlab.inf.unibz.it>
2021-05-28 15:12:15 +0200

    Test game saving.

---
+++ src/test/java/com/jubble/app/utils/SerializerTest.java
@@ -0,0 +1,28 @@
+package com.jubble.app.utils;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class SerializerTest {
+  @Test
+  public void shouldSerializeData() throws JsonProcessingException {
+    GameProgress progress = new GameProgress(List.of(1, 2, 4), 23);
+    String data = GameProgressHandler.serialize(progress);
+    assertThat(data)
+        .isEqualTo(
+            "    {\n"
+                + "      \"ownedGenerators\" : [ 1, 2, 4 ],\n"
+                + "      \"balance\" : 23.0\n"
+                + "    }");
+  }
+
+  @Test
+  public void shouldThrowIfNullIsPassed() {
+    Exception expected = assertThrows(Exception.class, () -> GameProgressHandler.serialize(null));
+    assertThat(expected.getMessage()).contains("Progress can't be null.");
+  }
+}
