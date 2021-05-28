5467afa678513ede1d70424ac8dc6a5f40ea8b3a
Alberto Defendi <1369-ahl-berto@users.noreply.gitlab.inf.unibz.it>
2021-05-28 15:11:41 +0200

    Mock data test for deserializer.

--- src/test/java/com/jubble/app/utils/GameProgressHandlerTest.java
+++ src/test/java/com/jubble/app/utils/GameProgressHandlerTest.java
@@ -1,62 +1,40 @@
 package com.jubble.app.utils;
 
-import static org.junit.Assert.*;
+import static com.google.common.truth.Truth.assertThat;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jubble.app.components.generator.Generator;
 import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GameProgressHandlerTest {
-  List<Generator> list = Settings.getGenerators();
-  GameProgress progress = new GameProgress(list, 0.0);
+  List<Integer> list =
+      Settings.getGenerators().stream().map(Generator::getNumberOwned).collect(Collectors.toList());
+  GameProgress progress = new GameProgress(List.of(1, 0, 0, 0, 0, 0), 0.0);
+  GameProgress generators;
+  String json;
+
+  @BeforeEach
+  void setup() throws JsonProcessingException {
+    json = "{\n" + "  \"ownedGenerators\" : [ 1, 0, 0, 0, 0, 0 ],\n" + "  \"balance\" : 0\n" + "}";
+    generators = new ObjectMapper().readValue(json, GameProgress.class);
+  }
 
   @Test
-  public void testMockDeserialization() throws JsonProcessingException {
-    String json =
-        "{\n"
-            + "  \"allOwnedGenerators\" : [ {\n"
-            + "    \"numberOwned\" : 0,\n"
-            + "    \"name\" : \"Stellar Panel\",\n"
-            + "    \"productionBase\" : 1.67,\n"
-            + "    \"nextCost\" : 3.738,\n"
-            + "    \"production\" : 0.0\n"
-            + "  }, {\n"
-            + "    \"numberOwned\" : 0,\n"
-            + "    \"name\" : \"Electron Absorber\",\n"
-            + "    \"productionBase\" : 20.0,\n"
-            + "    \"nextCost\" : 60.0,\n"
-            + "    \"production\" : 0.0\n"
-            + "  }, {\n"
-            + "    \"numberOwned\" : 0,\n"
-            + "    \"name\" : \"Nucleus Extractor\",\n"
-            + "    \"productionBase\" : 90.0,\n"
-            + "    \"nextCost\" : 720.0,\n"
-            + "    \"production\" : 0.0\n"
-            + "  }, {\n"
-            + "    \"numberOwned\" : 0,\n"
-            + "    \"name\" : \"Hydrogenator\",\n"
-            + "    \"productionBase\" : 360.0,\n"
-            + "    \"nextCost\" : 8640.0,\n"
-            + "    \"production\" : 0.0\n"
-            + "  }, {\n"
-            + "    \"numberOwned\" : 0,\n"
-            + "    \"name\" : \"Dyson Sphere\",\n"
-            + "    \"productionBase\" : 2160.0,\n"
-            + "    \"nextCost\" : 103680.0,\n"
-            + "    \"production\" : 0.0\n"
-            + "  }, {\n"
-            + "    \"numberOwned\" : 0,\n"
-            + "    \"name\" : \"Black Hole Reverser\",\n"
-            + "    \"productionBase\" : 6480.0,\n"
-            + "    \"nextCost\" : 1244160.0,\n"
-            + "    \"production\" : 0.0\n"
-            + "  } ],\n"
-            + "  \"balance\" : 0.0\n"
-            + "}";
+  public void mockShouldBeNotNull() {
+    assertThat(generators).isNotNull();
+  }
 
-    GameProgress generators = new ObjectMapper().readValue(json, GameProgress.class);
-    System.out.println(generators);
+  @Test
+  public void balanceShouldBeEqual() {
+    assertThat(generators.getBalance()).isEqualTo(progress.getBalance());
+  }
+
+  @Test
+  public void generatorsListShouldBeEqual() {
+    assertThat(generators.getOwnedGenerators()).isEqualTo(progress.getOwnedGenerators());
   }
 }
