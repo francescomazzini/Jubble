28f531255f4abad74b3affcc1ec7f87b4df67377
Alberto Defendi <1369-ahl-berto@users.noreply.gitlab.inf.unibz.it>
2021-05-29 16:19:43 +0200

    Test new Generator methods.

--- src/test/java/com/jubble/app/components/generator/GeneratorTest.java
+++ src/test/java/com/jubble/app/components/generator/GeneratorTest.java
@@ -1,6 +1,7 @@
 package com.jubble.app.components.generator;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,9 +11,22 @@ public class GeneratorTest {
       new Generator(new GeneratorID("Stellar Panel", "", 1), new GeneratorValues(3.738, 1.67, 1.07));
 
   @Test
-  @DisplayName("NumberOwned should be zero at beginning.")
-  public void numberOwnedShouldBeZeroAtBeginning() {
-    assertThat(generator.getNumberOwned()).isEqualTo(0);
+  @DisplayName("NumberOwned should be DEFAULT at beginning.")
+  public void numberOwnedShouldBeDEFAULTAtBeginning() {
+    assertThat(generator.getNumberOwned()).isEqualTo(Generator.DEFAULT_NUMBER_OWNED_GENERATORS);
+  }
+
+  @Test
+  public void shouldNeverUpdateNumberOwnedWhenNotDefault() {
+    generator.incrementNumberOwned();
+    assertThrows(IllegalOperationException.class, () -> generator.setNumberOwned(44));
+  }
+
+  @Test
+  void shouldUpdateNumberOwnedWhenDefault() {
+    final int N = 100;
+    generator.setNumberOwned(N);
+    assertThat(generator.getNumberOwned()).isEqualTo(N);
   }
 
   @Test
