957345d6eb12173e095ee323b7934925dfa2b794
Francesco <francesco.mazzini432@gmail.com>
2021-05-30 15:51:44 +0200

    Generalization Made; Buttons Still Missing

--- src/main/java/com/jubble/app/javafx/ControllerFX.java
+++ src/main/java/com/jubble/app/javafx/ControllerFX.java
@@ -2,16 +2,15 @@ package com.jubble.app.javafx;
 
 import com.jubble.app.ThreadRunner;
 import com.jubble.app.components.Balance;
+import com.jubble.app.javafx.pages.bodies.BodyGenerators;
 import com.jubble.app.javafx.tasks.BalanceTask;
 import com.jubble.app.javafx.tasks.CostNextTask;
 import com.jubble.app.javafx.tasks.NumberOwnedTask;
 import com.jubble.app.javafx.tasks.ProductionTask;
 import com.jubble.app.utils.Settings;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.util.*;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -20,6 +19,7 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
@@ -35,10 +35,16 @@ public class ControllerFX implements Initializable {
 
   private List<ImageView> generatorImageViews;
 
+  private Map<String, VBox> bodyPages;
+
   /**
    * Each of the following variables refers to an existing javafx object which is contained in FXML
    * file and it has an ID equals to the name of these variables
    */
+  @FXML private FlowPane main_page_container;
+
+  @FXML private VBox main_body;
+
   @FXML private Label balanceLabel;
 
   @FXML private VBox shopPanel;
@@ -75,7 +81,8 @@ public class ControllerFX implements Initializable {
    */
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
-    ThreadTaskUtil threads = new ThreadTaskUtil();
+
+    bodyPages = new HashMap<>();
 
     generatorLabels = new ArrayList<>();
     generatorImageViews = new ArrayList<>();
@@ -86,30 +93,34 @@ public class ControllerFX implements Initializable {
 
     generatePageGenerator();
 
+    main_body.getChildren().clear();
+    main_body.getChildren().add(bodyPages.get("page00"));
+
     BalanceTask balTask = new BalanceTask();
 
     balanceLabel.textProperty().bind(balTask.messageProperty());
 
-    threads.autoBuild(balTask);
+    ThreadTaskUtil.autoBuild(balTask);
 
     ProductionTask prodTask = new ProductionTask();
     totalProductionLabel.textProperty().bind(prodTask.messageProperty());
 
-    threads.autoBuild(prodTask);
+    ThreadTaskUtil.autoBuild(prodTask);
 
     for (int i = 0; i < generatorLabels.size(); i++) {
       CostNextTask costTask = new CostNextTask(i);
 
       generatorLabels.get(i).get(2).textProperty().bind(costTask.messageProperty());
 
-      threads.autoBuild(costTask);
+      ThreadTaskUtil.autoBuild(costTask);
 
+      /*
       NumberOwnedTask nrTask =
           new NumberOwnedTask(i, generatorImageViews.get(i), generatorLabels.get(i).get(3));
 
       generatorLabels.get(i).get(3).textProperty().bind(nrTask.messageProperty());
 
-      threads.autoBuild(nrTask);
+      ThreadTaskUtil.autoBuild(nrTask); */
 
     }
   }
@@ -164,8 +175,10 @@ public class ControllerFX implements Initializable {
 
     int length = Settings.getGenerators().size();
 
-    final int maxPerRow = 3;
+    final int max = BodyGenerators.NR_MAX_GENERATORS_PER_PAGE;
 
+    int counter = 0;
+/*
     for (int i = 0; i < length; i++) {
 
       Label numberOwned = new Label("Nr: " + Settings.getGenerators().get(i).getNumberOwned());
@@ -186,6 +199,37 @@ public class ControllerFX implements Initializable {
       generatorLabels.get(i).add(numberOwned);
 
       pageGrid.add(vbx, (i < maxPerRow ? maxPerRow - 1 : 0), (i % maxPerRow));
+    } */
+
+    int maxItimes = ((length / max) + ((length % max == 0) ? 0 : 1));
+
+    for (int i = 0; i < maxItimes; i++) {
+
+      BodyGenerators body = new BodyGenerators();
+
+      /**
+       * maxItimes is the number of pages of generators needed when generators number is more than
+       * max that is NR_MAX_GENERATORS_PER_PAGE
+       *
+       * Therefore the outer cycle represents page creation.
+       *
+       * Inner cycle is the generator creation which must repeat NR_MAX_GENERATORS_PER_PAGE if the
+       * number of generators for that page is sufficient to fill it completely or it must repeat
+       * only for less times that would be the number of the generators left to put.
+       * This case happens when numberOfGenerators % NR_MAX_GENERATORS_PER_PAGE != 0
+       *
+       */
+      for(int j = 0; j < ((((maxItimes == (length / max)) ? maxItimes :(maxItimes - 1)) == i) ? (length % max) : max); j++) {
+
+        body.addGenerator(counter, "assets/game-components/generator" + ((counter > 9) ? counter : ("0" + counter)) + ".png");
+
+        counter++;
+      }
+
+      body.buildPage();
+
+      bodyPages.put(("page" + ((i > 9) ? i : ("0" + i))), body);
+
     }
   }
 
