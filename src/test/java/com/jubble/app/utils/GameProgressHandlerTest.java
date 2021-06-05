9c7e86a18ce9a5d812594581f0ddd123df026ce6
Alberto Defendi <1369-ahl-berto@users.noreply.gitlab.inf.unibz.it>
2021-06-05 11:58:57 +0200

    Remove not used list and fix error.

--- src/test/java/com/jubble/app/utils/GameProgressHandlerTest.java
+++ src/test/java/com/jubble/app/utils/GameProgressHandlerTest.java
@@ -4,18 +4,12 @@ import static com.google.common.truth.Truth.assertThat;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jubble.app.components.generator.Generator;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GameProgressHandlerTest {
-  List<Integer> list =
-      Assets.getInstance().getGenerators().stream()
-          .map(Generator::getNumberOwned)
-          .collect(Collectors.toList());
-  GameProgress progress = new GameProgress(List.of(1, 0, 0, 0, 0, 0), 0.0);
+ GameProgress progress = new GameProgress(List.of(1, 0, 0, 0, 0, 0), 0.0);
   GameProgress generators;
   String json;
 
