f6cc0ddf5b52c5db172de548e849978f463d63a4
Alberto Defendi <1369-ahl-berto@users.noreply.gitlab.inf.unibz.it>
2021-06-05 11:59:37 +0200

    Remove not used.

--- src/main/java/com/jubble/app/javafx/ControllerFX.java
+++ src/main/java/com/jubble/app/javafx/ControllerFX.java
@@ -15,7 +15,6 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 
