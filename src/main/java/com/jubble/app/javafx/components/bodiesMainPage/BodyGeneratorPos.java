8eb6f398dbb9775b115427f4647bf24807eed242
Alberto Defendi <1369-ahl-berto@users.noreply.gitlab.inf.unibz.it>
2021-06-06 11:08:08 +0200

    Move generator constants here.

---
+++ src/main/java/com/jubble/app/javafx/components/bodiesMainPage/BodyGeneratorPos.java
@@ -0,0 +1,18 @@
+package com.jubble.app.javafx.components.bodiesMainPage;
+
+public enum BodyGeneratorPos {
+    ROW_MAX(2),
+    COLUMN_MAX(3),
+    PAGE_MAX(6);
+
+    private final int value;
+
+    public int value() {
+        return value;
+    }
+
+    BodyGeneratorPos(int value) {
+        this.value = value;
+    }
+
+}
