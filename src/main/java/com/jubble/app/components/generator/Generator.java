0cc3aead58c0b2211457bd8933cd83ed7285aa75
Alberto Defendi <1369-ahl-berto@users.noreply.gitlab.inf.unibz.it>
2021-06-07 10:55:58 +0200

    Implement builder pattern.

--- src/main/java/com/jubble/app/components/generator/Generator.java
+++ src/main/java/com/jubble/app/components/generator/Generator.java
@@ -1,57 +1,124 @@
 package com.jubble.app.components.generator;
 
-public class Generator extends GeneratorID {
-  private GeneratorValues values;
-  public static final int DEFAULT_NUMBER_OWNED_GENERATORS = 0;
-  private int numberOwned;
+public class Generator {
+    /**
+     * Represents the name of this generator.
+     */
+    private final String name;
+    /**
+     * Represents the description of this generator.
+     */
+    private final String description;
+    /**
+     * Represents a level (or progress) in the game.
+     */
+    private final int level;
+    private final double costBase;
+    private final double productionBase;
+    private final double rateGrowth;
+    public static final int DEFAULT_NUMBER_OWNED_GENERATORS = 0;
+    private int numberOwned;
 
-  public Generator(GeneratorID id, GeneratorValues values) {
-    super(id);
-    // The player does not own any generator at beginning of the game.
-    numberOwned = DEFAULT_NUMBER_OWNED_GENERATORS;
-    this.values = values;
-  }
+    private Generator(Builder builder) {
+        name = builder.name;
+        description = builder.description;
+        level = builder.level;
+        costBase = builder.costBase;
+        productionBase = builder.productionBase;
+        rateGrowth = builder.rateGrowth;
+        numberOwned = builder.numberOwned;
+    }
 
-  public void incrementNumberOwned() {
-    numberOwned++;
-  }
+    public static class Builder {
+        // Optional parameters - initialized to default values.
+        private String name = "";
+        private String description = "";
+        private int level = 0;
+        private double costBase = 0d;
+        private double productionBase = 0d;
+        private double rateGrowth = 0d;
+        private int numberOwned = DEFAULT_NUMBER_OWNED_GENERATORS;
 
-  public int getNumberOwned() {
-    return numberOwned;
-  }
+        public Builder name(String val) {
+            name = val;
+            return this;
+        }
 
-  public void setNumberOwned(int numberOwned) {
-    IllegalOperationException.checkIfNumberOwnedIsDefault(this.numberOwned);
-    this.numberOwned = numberOwned;
-  }
+        public Builder description(String val) {
+            description = val;
+            return this;
+        }
 
-  public double getNextCost() {
-    return values.getCostBase() * Math.pow(values.getRateGrowth(), numberOwned);
-  }
+        public Builder level(int val) {
+            level = val;
+            return this;
+        }
 
-  public double getProduction() {
-    return (values.getProductionBase() * numberOwned) * super.getLevel();
-  }
+        public Builder costBase(double val) {
+            costBase = val;
+            return this;
+        }
 
-  public double getProductionBase() {
-    return values.getProductionBase();
-  }
+        public Builder productionBase(double val) {
+            productionBase = val;
+            return this;
+        }
 
-  public String getName() {
-    return super.getName();
-  }
+        public Builder rateGrowth(double val) {
+            rateGrowth = val;
+            return this;
+        }
 
-  /**
-   * Tells if numberOwned is default.
-   *
-   * @return true if numberOwned is default.
-   */
-  public boolean isMoreThanZeroOwned() {
-    return numberOwned != DEFAULT_NUMBER_OWNED_GENERATORS;
-  }
+        public Builder numberOwned(int val) {
+            numberOwned = val;
+            return this;
+        }
 
-  @Override
-  public String toString() {
-    return super.toString() + " " + numberOwned;
-  }
+        public Generator build() {
+            return new Generator(this);
+        }
+    }
+
+    public void incrementNumberOwned() {
+        numberOwned++;
+    }
+
+    public int getNumberOwned() {
+        return numberOwned;
+    }
+
+    public void setNumberOwned(int numberOwned) {
+        IllegalOperationException.checkIfNumberOwnedIsDefault(this.numberOwned);
+        this.numberOwned = numberOwned;
+    }
+
+    public double getNextCost() {
+        return costBase * Math.pow(rateGrowth, numberOwned);
+    }
+
+    public double getProduction() {
+        return (productionBase * numberOwned) * level;
+    }
+
+    public double getProductionBase() {
+        return productionBase;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Tells if numberOwned is more than default value 0.
+     *
+     * @return true if numberOwned is different than default.
+     */
+    public boolean isMoreThanZeroOwned() {
+        return numberOwned != DEFAULT_NUMBER_OWNED_GENERATORS;
+    }
+
+    @Override
+    public String toString() {
+        return "Generator: " + name + " level: " + level + " number owned: " + numberOwned;
+    }
 }
