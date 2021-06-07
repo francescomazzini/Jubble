15244e8bee83c0d75a1489282d034f7dabe2d12e
Alberto Defendi <1369-ahl-berto@users.noreply.gitlab.inf.unibz.it>
2021-06-07 07:57:10 +0200

    Inject dependencies and create methods.

--- src/main/java/com/jubble/app/javafx/components/BalanceFX.java
+++ src/main/java/com/jubble/app/javafx/components/BalanceFX.java
@@ -5,15 +5,29 @@ import com.jubble.app.javafx.tasks.BalanceTask;
 import com.jubble.app.javafx.tasks.ProductionTask;
 import javafx.scene.control.Label;
 
-public class BalanceFX {
-  public BalanceFX(Label balanceLabel, Label totalProductionLabel) {
+import java.util.Objects;
 
+public class BalanceFX {
+  private final Label balanceLabel;
+  private final Label totalProductionLabel;
+
+  public BalanceFX(Label balanceLabel, Label totalProductionLabel) {
+    this.balanceLabel = Objects.requireNonNull(balanceLabel);
+    this.totalProductionLabel = Objects.requireNonNull(totalProductionLabel);
+    buildBalanceTask();
+    buildProductionTask();
+  }
+
+  private void buildBalanceTask() {
     BalanceTask balTask = new BalanceTask();
     balanceLabel.textProperty().bind(balTask.messageProperty());
     ThreadTaskUtil.autoBuild(balTask);
+  }
 
+  private void buildProductionTask() {
     ProductionTask prodTask = new ProductionTask();
     totalProductionLabel.textProperty().bind(prodTask.messageProperty());
     ThreadTaskUtil.autoBuild(prodTask);
   }
+
 }
