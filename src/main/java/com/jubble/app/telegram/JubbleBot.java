15c8ea45621be5d326a8d7a62396605ea0bf8ec2
Francesco <francesco.mazzini432@gmail.com>
2021-06-08 23:51:41 +0200

    Telegram Tries

--- src/main/java/com/jubble/app/telegram/JubbleBot.java
+++ src/main/java/com/jubble/app/telegram/JubbleBot.java
@@ -7,10 +7,16 @@ import com.jubble.app.components.generator.GeneratorsSingleton;
 import com.jubble.app.utils.GameProgressHandler;
 import com.jubble.app.utils.NumberNames;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.ParseMode;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JubbleBot extends TelegramLongPollingBot {
 
@@ -33,72 +39,125 @@ public class JubbleBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
 
+        String stringMessage = "";
         SendMessage message = new SendMessage();
-        String command = update.getMessage().getText();
-        Message chatUser = update.getMessage();
 
-        if(command.equals("/start")) {
+        if(update.hasCallbackQuery()) {
+            // Set variables
 
-            message.setText("Hi " + chatUser.getFrom().getUserName() + ", welcome to JUBBLE!" +
-                    "\nType \"/begin\" to start the game!" +
-                    "\nType \"/sotp\" to stop the game!");
+            String call_data = update.getCallbackQuery().getData();
 
-        } else if(command.equals("/begin")) {
+            if (call_data.equals("status_inline")) {
 
-            if(!isGameOn) {
-                message.setText("Hi " + chatUser.getFrom().getUserName() + ", the game has started");
-
-                GameProgressHandler.loadGame();
-                ThreadRunner.run();
-
-                setGameOn(true);
-            } else
-                message.setText("The game is already running");
-
-
-        } else if(command.equals("/status")) {
-
-            if(isGameOn) {
-                String stringMessage = ("STATUS: " +
-                        "\n Balance: " + NumberNames.createString(Balance.getPrimary()) +
-                        "\n Current Total Production: " +
-                            NumberNames.createString(GeneratorsSingleton.getGenerators()
-                                                                        .stream()
-                                                                        .mapToDouble(Generator::getProduction)
-                                                                        .sum())
+                stringMessage = ("*STATUS*: " +
+                        "\n *Balance*: " + NumberNames.createString(Balance.getPrimary()) +
+                        "\n *Total Production*: " +
+                        NumberNames.createString(GeneratorsSingleton.getGenerators()
+                                .stream()
+                                .mapToDouble(Generator::getProduction)
+                                .sum())
                         + " / s" +
-                        "\n\n Generators Owned: ");
+                        "\n\n *Generators Owned*: ");
 
                 for(Generator gen : GeneratorsSingleton.getGenerators()) {
-                    stringMessage += "\n   - " + gen.getName() + ": " + gen.getNumberOwned();
+                    stringMessage += "\n   • " + gen.getName() + ": " + gen.getNumberOwned();
                 }
 
-                message.setText(stringMessage);
-
-            } else
-                message.setText("The game is not running");
+            }
+        } else {
 
 
-        } else if (command.equals("/stop")) {
+            String command = update.getMessage().getText();
+            Message chatUser = update.getMessage();
 
-            if(isGameOn) {
-                message.setText("Thank you " + chatUser.getFrom().getUserName() + ", the game stopped");
+            if (command.equals("/start")) {
 
-                ThreadRunner.stop();
+                stringMessage = "Hi " + chatUser.getFrom().getUserName() + ", welcome to JUBBLE!" +
+                        "\nType \"/begin\" to start the game!" +
+                        "\nType \"/sotp\" to stop the game!";
 
-                setGameOn(false);
-            } else
-                message.setText("The game is not running yet");
+            } else if (command.equals("/begin")) {
 
+                if (!isGameOn) {
+                    stringMessage = "Hi " + chatUser.getFrom().getUserName() + ", the game has started";
+
+                    GameProgressHandler.loadGame();
+                    ThreadRunner.run();
+
+                    createInlineKeyboard(message);
+
+                    setGameOn(true);
+                } else
+                    stringMessage = "The game is already running";
+
+
+            } else if (command.equals("/status")) {
+
+                if (isGameOn) {
+                    stringMessage = ("*STATUS*: " +
+                            "\n *Balance*: " + NumberNames.createString(Balance.getPrimary()) +
+                            "\n *Total Production*: " +
+                            NumberNames.createString(GeneratorsSingleton.getGenerators()
+                                    .stream()
+                                    .mapToDouble(Generator::getProduction)
+                                    .sum())
+                            + " / s" +
+                            "\n\n *Generators Owned*: ");
+
+                    for (Generator gen : GeneratorsSingleton.getGenerators()) {
+                        stringMessage += "\n   • " + gen.getName() + ": " + gen.getNumberOwned();
+                    }
+
+
+                } else
+                    stringMessage = "The game is not running";
+
+
+            } else if (command.equals("/stop")) {
+
+                if (isGameOn) {
+                    stringMessage = "Thank you " + chatUser.getFrom().getUserName() + ", the game stopped";
+
+                    ThreadRunner.stop();
+
+                    setGameOn(false);
+                } else
+                    stringMessage = "The game is not running yet";
+
+            }
         }
 
-        message.setChatId(chatUser.getChatId() + "");
+
+        message.setText(stringMessage);
+        message.setParseMode(ParseMode.MARKDOWN);
+
+        if(update.hasCallbackQuery())
+            message.setChatId(update.getCallbackQuery().getMessage().getChatId() + "");
+        else
+            message.setChatId(update.getMessage().getChatId() + "");
 
         try {
             execute(message);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+    }
+
+    public void createInlineKeyboard (SendMessage message) {
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+        List<InlineKeyboardButton> rowInline = new ArrayList<>();
+
+        InlineKeyboardButton button = new InlineKeyboardButton();
+        button.setText("Status");
+        button.setCallbackData("status_inline");
+
+        rowInline.add(button);
+        // Set the keyboard to the markup
+        rowsInline.add(rowInline);
+        // Add it to the message
+        markupInline.setKeyboard(rowsInline);
+        message.setReplyMarkup(markupInline);
     }
 
 
