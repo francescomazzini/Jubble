a7c6c4a3506b9c2b10efd46614e4c070fe7ef648
Alberto Defendi <1369-ahl-berto@users.noreply.gitlab.inf.unibz.it>
2021-06-09 12:21:24 +0200

    Add annotation to deserialize data.

--- src/main/java/com/jubble/app/components/generator/Generator.java
+++ src/main/java/com/jubble/app/components/generator/Generator.java
@@ -1,14 +1,22 @@
 package com.jubble.app.components.generator;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
 /**
  * Represents a generator in the game.
  * Implements the builder pattern and can be instantiated only providing a builder.
  */
+@JsonDeserialize(builder = Generator.Builder.class)
 public class Generator extends GeneratorBody {
 
     private Generator(Builder builder) {
         super(builder);
-   }
+    }
+
+    @JsonPOJOBuilder(withPrefix = "")
     public static class Builder extends GeneratorBody.Builder<Builder> {
 
         @Override
@@ -17,13 +25,16 @@ public class Generator extends GeneratorBody {
         }
 
         @Override
-        protected Builder self() { return this; }
+        protected Builder self() {
+            return this;
+        }
     }
 
     public void incrementNumberOwned() {
-        super.setNumberOwned(getNumberOwned()+1);
+        super.setNumberOwned(getNumberOwned() + 1);
     }
 
+    @JsonIgnore
     public int getNumberOwned() {
         return super.getNumberOwned();
     }
@@ -33,14 +44,17 @@ public class Generator extends GeneratorBody {
         super.setNumberOwned(super.getNumberOwned());
     }
 
+    @JsonIgnore
     public double getNextCost() {
         return super.getCostBase() * Math.pow(super.getRateGrowth(), super.getNumberOwned());
     }
 
+    @JsonIgnore
     public double getProduction() {
         return (super.getProductionBase() * super.getNumberOwned()) * super.getLevel();
     }
 
+    @JsonIgnore
     public double getProductionBase() {
         return super.getProductionBase();
     }
@@ -54,6 +68,7 @@ public class Generator extends GeneratorBody {
      *
      * @return true if numberOwned is different than default.
      */
+    @JsonIgnore
     public boolean isMoreThanZeroOwned() {
         return getNumberOwned() != DEFAULT_NUMBER_OWNED_GENERATORS;
     }
