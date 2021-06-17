c7e95ff3a3de26c6adb8c62f8ed674ae47148bb4
Alberto Defendi <1369-ahl-berto@users.noreply.gitlab.inf.unibz.it>
2021-06-17 08:40:04 +0200

    Move actions to BotAction class.

--- src/main/java/com/jubble/app/telegram/JubbleBot.java
+++ src/main/java/com/jubble/app/telegram/JubbleBot.java
@@ -1,9 +1,5 @@
 package com.jubble.app.telegram;
 
-import com.jubble.app.core.GameActions;
-import com.jubble.app.core.Settings;
-import com.jubble.app.core.resources.generator.Generator;
-import com.jubble.app.core.threads.ThreadRunner;
 import com.jubble.app.telegram.elements.MessageContent;
 import com.jubble.app.telegram.elements.TelegramMessage;
 import com.jubble.app.telegram.elements.TypeMessages;
@@ -102,28 +98,23 @@ public final class JubbleBot extends TelegramLongPollingBot {
   private void actionPerformer(final String action, final TelegramMessage message) {
     Objects.requireNonNull(action);
     Objects.requireNonNull(message);
+
+    BotActions actionPerformer = new BotActions(message);
+
     if (action.equals(MessageContent.CHOSE_OPTIONS.getAction()) && !isGameOn) {
-      message.setContent("The *game* is running.\n" + message.getContent());
-      ThreadRunner.run();
+      actionPerformer.start();
       isGameOn = true;
     }
     if (action.equals(MessageContent.STOP_GAME.getAction()) && isGameOn) {
-      ThreadRunner.stop();
+      actionPerformer.stop();
       isGameOn = false;
     } else {
       if (action.equals(MessageContent.STATUS.getAction())) {
-        message.setText(message.getContent() + TypeMessages.generateStatusContent());
+        actionPerformer.openStatus();
       } else if (action.equals(MessageContent.OPEN_SHOP.getAction())) {
-        message.setText(message.getContent() + TypeMessages.generateShopContent());
-        message.setInlineButtons(TypeMessages.generateShopButtons());
+        actionPerformer.openShop();
       } else if (action.startsWith(MessageContent.CHECK_BALANCE.getAction())) {
-        int numGenerator = Integer.parseInt(action.substring(3));
-        Generator gen = Settings.getGeneratorList().get(numGenerator);
-        boolean isBought = GameActions.buyGenerator(gen);
-
-        if (isBought)
-          message.setText(message.getContent() + " successfully bought " + gen.getName() + " ✅ ");
-        else message.setText(message.getContent() + " not enough money" + " \uD83D\uDCDB ");
+        actionPerformer.openBalance(action);
       }
     }
   }
