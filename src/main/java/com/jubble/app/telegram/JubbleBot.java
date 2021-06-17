fc239e3ff0b1509e877ec8de373ee58e60272e7c
Alberto Defendi <1369-ahl-berto@users.noreply.gitlab.inf.unibz.it>
2021-06-17 08:23:51 +0200

    Better conditions.
    See #5.

--- src/main/java/com/jubble/app/telegram/JubbleBot.java
+++ src/main/java/com/jubble/app/telegram/JubbleBot.java
@@ -15,11 +15,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public final class JubbleBot extends TelegramLongPollingBot {
 
-  /**
-   * listMessages map is a Map that for each type of TelegramMessage (already written in
-   * TypeMessages) has the callBackQuery as key. In this way only one TelegramMessage correspond to
-   * a callBackQuery.
-   */
+  /** Maps the command name (callBackQuery) with each TelegramMessage. */
   private final Map<String, TelegramMessage> listMessages = TypeMessages.listOfMessages;
 
   private boolean isGameOn;
@@ -59,7 +55,7 @@ public final class JubbleBot extends TelegramLongPollingBot {
     }
   }
 
-  private String getReceivedCommand(Update update) {
+  private String getCommandFrom(Update update) {
     if (update.hasCallbackQuery()) return update.getCallbackQuery().getData();
     else return update.getMessage().getText();
   }
@@ -68,69 +64,67 @@ public final class JubbleBot extends TelegramLongPollingBot {
     return listMessages.get(command);
   }
 
+  private String extractGeneratorCommand(String command) {
+    return command.substring(0, 3);
+  }
+
   private TelegramMessage getSubMenu(String command) {
-    String tempCommand = command.substring(0, 3);
+    String tempCommand = extractGeneratorCommand(command);
     return listMessages.get(tempCommand);
   }
 
   /**
-   * Each time that the bot receives an update, it analyzes the command received. All the commands
-   * are defined in {@link com.jubble.app.telegram.elements.MessageContent} Two types of updates
-   * exist, that are handled equally by this method: - a command: ex. "/start" - a callBackQuery:
-   * ex. "shop". so it just put it in a string. Then it search in the map the correct
-   * TelegramMessage to use it to answer to the command given.
+   * All the commands are defined in {@link com.jubble.app.telegram.elements.MessageContent} Two
+   * types of updates exist, that are handled equally by this method: - a command: ex. "/start" - a
+   * callBackQuery: ex. "shop". so it just put it in a string.
    *
    * <p>There can be cases where the command is not found and it could be because the commands to
    * buy generators are all wrapped under "gen" command and then followed by the number of the
-   * generator they refer to. Because of this there is an if which checks if this is the case. If it
-   * is not even the command obviously is not run
+   * generator they refer to.
    *
-   * @param update is the object given by API when the user interacts with the bot
+   * @param update is the object given by API when the user interacts with the bot.
    */
   @Override
   public void onUpdateReceived(Update update) {
     Objects.requireNonNull(update);
-    String command = getReceivedCommand(update);
-    TelegramMessage telegramMessage =
+    String command = getCommandFrom(update);
+    TelegramMessage message =
         !Objects.isNull(getMenu(command)) ? getMenu(command) : getSubMenu(command);
-    tryToExecuteAction(telegramMessage, command, update);
+    tryToExecuteAction(message, command, update);
   }
 
   /**
-   * ActionPerformer runs snippet of code modifying TelegramMessage depending on the command
-   * inserted. This is needed because some information are generated or executed at run time
+   * Runs the method linked to each command.
    *
-   * @param action that is the command given by the update
-   * @param tlMsg is the TelegramMessage which correspond to the key command in the Map
+   * @param action command given by the update.
+   * @param message TelegramMessage which correspond to the key command in the Map.
    */
-  private void actionPerformer(String action, TelegramMessage tlMsg) {
+  private void actionPerformer(final String action, final TelegramMessage message) {
+    Objects.requireNonNull(action);
+    Objects.requireNonNull(message);
     if (action.equals(MessageContent.CHOSE_OPTIONS.getAction()) && !isGameOn) {
-      tlMsg.setContent("The *game* is running.\n" + tlMsg.getContent());
+      message.setContent("The *game* is running.\n" + message.getContent());
       ThreadRunner.run();
       isGameOn = true;
     }
-
     if (action.equals(MessageContent.STOP_GAME.getAction()) && isGameOn) {
       ThreadRunner.stop();
       isGameOn = false;
-    }
+    } else {
+      if (action.equals(MessageContent.STATUS.getAction())) {
+        message.setText(message.getContent() + TypeMessages.generateStatusContent());
+      } else if (action.equals(MessageContent.OPEN_SHOP.getAction())) {
+        message.setText(message.getContent() + TypeMessages.generateShopContent());
+        message.setInlineButtons(TypeMessages.generateShopButtons());
+      } else if (action.startsWith(MessageContent.CHECK_BALANCE.getAction())) {
+        int numGenerator = Integer.parseInt(action.substring(3));
+        Generator gen = Settings.getGeneratorList().get(numGenerator);
+        boolean isBought = GameActions.buyGenerator(gen);
 
-    if (action.equals(MessageContent.STATUS.getAction()))
-      tlMsg.setText(tlMsg.getContent() + TypeMessages.generateStatusContent());
-
-    if (action.equals(MessageContent.OPEN_SHOP.getAction())) {
-      tlMsg.setText(tlMsg.getContent() + TypeMessages.generateShopContent());
-      tlMsg.setInlineButtons(TypeMessages.generateShopButtons(), 5);
-    }
-
-    if (action.startsWith(MessageContent.CHECK_BALANCE.getAction())) {
-      int numGenerator = Integer.parseInt(action.substring(3));
-      Generator gen = Settings.getGeneratorList().get(numGenerator);
-      boolean isBought = GameActions.buyGenerator(gen);
-
-      if (isBought)
-        tlMsg.setText(tlMsg.getContent() + " successfully bought " + gen.getName() + " ✅ ");
-      else tlMsg.setText(tlMsg.getContent() + " not enough money" + " \uD83D\uDCDB ");
+        if (isBought)
+          message.setText(message.getContent() + " successfully bought " + gen.getName() + " ✅ ");
+        else message.setText(message.getContent() + " not enough money" + " \uD83D\uDCDB ");
+      }
     }
   }
 }
