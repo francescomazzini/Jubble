d5462758820b979da92db3e7c504ba0892bb2447
Alberto Defendi <1369-ahl-berto@users.noreply.gitlab.inf.unibz.it>
2021-06-18 14:13:07 +0200

    Rename folder.

--- src/main/java/com/jubble/app/telegram/messages/TelegramMessage.java
+++ src/main/java/com/jubble/app/telegram/messages/TelegramMessage.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+
 import org.telegram.telegrambots.meta.api.methods.ParseMode;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
