0029cae6bf746feb2de684f5a96d910c2aef4cb0
Francesco <francesco.mazzini432@gmail.com>
2021-06-18 10:33:07 +0200

    Add ThreadRunner Test

---
+++ src/test/java/com/jubble/app/core/threads/ThreadRunnerTest.java
@@ -0,0 +1,52 @@
+package com.jubble.app.core.threads;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.jubble.app.core.GameActions;
+import com.jubble.app.core.Settings;
+import com.jubble.app.core.resources.Currency;
+import com.jubble.app.core.resources.generator.Generator;
+import java.util.List;
+import java.util.Timer;
+import org.junit.jupiter.api.*;
+
+public class ThreadRunnerTest {
+
+  Timer VALUES_TIMER;
+  static List<Generator> generatorList;
+  static Currency balance;
+  final int timerRefreshRate = 10;
+  final int timerDelay = 0;
+
+  @BeforeAll
+  static void setUp() {
+    generatorList = Settings.getGeneratorList();
+  }
+
+  /*
+  It is 10ms while actually should be 1000 because the tests must run fast!
+   */
+  @RepeatedTest(4)
+  @Test
+  @DisplayName("Timer Should Update Balance Value Every Tot Set ms")
+  public void timerShouldUpdateBalanceEveryTotSeconds() throws InterruptedException {
+
+    VALUES_TIMER = new Timer();
+    balance = Settings.getCurrencyList().get(0);
+    VALUES_TIMER.schedule(new GameValuesThread(), timerDelay, timerRefreshRate);
+
+    double initialValue = balance.getOwned();
+
+    Thread.sleep(timerRefreshRate * 2);
+
+    if (GameActions.getSumTotalProductionGenerators() != 0)
+      assertThat(balance.getOwned())
+          .isGreaterThan(initialValue + GameActions.getSumTotalProductionGenerators());
+    else
+      assertThat(balance.getOwned())
+          .isEqualTo(initialValue + GameActions.getSumTotalProductionGenerators());
+
+    VALUES_TIMER.cancel();
+    VALUES_TIMER.purge();
+  }
+}
